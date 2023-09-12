8d5a7cfe83830745ed34c9290c298fc155a462a3
ArthurOS <arth3608@stud.kea.dk>
2023-09-12 14:08:33 +0200

    opgave 1 done

--- hello.js
+++ hello.js
@@ -1,14 +1,39 @@
-fetch("https://jsonplaceholder.typicode.com/users/",{
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        name: 'User 1',
-    })
-})
-    .then(res =>{
-        return res.json();
-})
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+document.addEventListener("DOMContentLoaded", function () {
+    // Your code here
+    document.querySelector("form").addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const userId = document.querySelector("#user").value;
+
+        fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+            .then((res) => res.json())
+            .then((data) => {
+                document.querySelector("#userName").textContent = data.name;
+                document.querySelector("#userPhone").textContent = data.phone;
+                document.querySelector("#userZip").textContent = data.address.zipcode;
+                document.querySelector("#userCity").textContent = data.address.city;
+                document.querySelector("#userStreet").textContent = data.address.street;
+                document.querySelector("#userGeo").textContent = `${data.address.geo.lat}, ${data.address.geo.lng}`;
+            })
+            .catch((err) => console.log(err));
+    });
+    document.querySelector("#getAllBtn".addEventListener("click", function (e){
+        e.preventDefault();
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+        .then((res) => res.json())
+        .then((data) => {
+            const userList = document.querySelector("#userList");
+
+
+            userList.innerHTML = "";
+
+            data.forEach((user) => {
+                const listItem = document.createElement("li");
+                listItem.textContent = "${user.name} (${user.phone})";
+                userList.appendChild(listItem);
+            });
+
+        })
+        .catch((err) => console.log(err));
+    }))
+});
